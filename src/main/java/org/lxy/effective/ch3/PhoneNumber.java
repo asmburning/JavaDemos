7a0e33ae2d4eadb09b0ea9954c4b3fa8f20a3fdf
Eric.Liu <826102786@qq.com>
2018-01-27 18:21:24 +0800

    effective ch3 compare

---
+++ src/main/java/org/lxy/effective/ch3/PhoneNumber.java
@@ -0,0 +1,41 @@
+package org.lxy.effective.ch3;
+
+import java.util.Comparator;
+
+public class PhoneNumber {
+    private final short areaCode, prefix, lineNum;
+
+    public PhoneNumber(int areaCode, int prefix, int lineNum) {
+        this.areaCode = rangeCheck(areaCode, 999, "area code");
+        this.prefix = rangeCheck(prefix, 999, "prefix");
+        this.lineNum = rangeCheck(lineNum, 9999, "line num");
+    }
+
+    private static short rangeCheck(int val, int max, String arg) {
+        if (val < 0 || val > max)
+            throw new IllegalArgumentException(arg + ": " + val);
+        return (short) val;
+    }
+
+    // Multiple-field Comparable with primitive fields
+    public int compareToOld(PhoneNumber pn) {
+        int result = Short.compare(areaCode, pn.areaCode);
+        if (result == 0) {
+            result = Short.compare(prefix, pn.prefix);
+            if (result == 0)
+                result = Short.compare(lineNum, pn.lineNum);
+        }
+        return result;
+    }
+
+    // Comparable with comparator construction methods
+    private static final Comparator<PhoneNumber> COMPARATOR =
+            Comparator.comparingInt((PhoneNumber pn) -> pn.areaCode)
+                    .thenComparingInt(pn -> pn.prefix)
+                    .thenComparingInt(pn -> pn.lineNum);
+
+    public int compareTo(PhoneNumber pn) {
+        return COMPARATOR.compare(this, pn);
+    }
+
+}
