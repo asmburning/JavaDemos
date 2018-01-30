6242d447aac6439a0c7fcdc39cc71b789a8c383d
XinyiLiu <Xinyi.liu@quarkfinance.com>
2018-01-30 15:07:55 +0800

    cookbook ch1

---
+++ src/main/java/org/lxy/cookbook/ch1/Http2Test.java
@@ -0,0 +1,22 @@
+package org.lxy.cookbook.ch1;
+
+import org.junit.Test;
+
+public class Http2Test {
+
+    @Test
+    public void test() throws Exception {
+
+        /*HttpClient client = HttpClient.newBuilder().build();
+        HttpRequest request = HttpRequest
+                .newBuilder(new URI("http://httpbin.org/get"))
+                .GET()
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandler.asString());
+        System.out.println("Status code: " + response.statusCode());
+        System.out.println("Response Body: " + response.body());*/
+    }
+
+}
---
+++ src/main/java/org/lxy/cookbook/ch1/ProcessHandlerTest.java
@@ -0,0 +1,16 @@
+package org.lxy.cookbook.ch1;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class ProcessHandlerTest {
+
+    @Test
+    public void test1() {
+        ProcessHandle currentProcess = ProcessHandle.current();
+        log.info("PID: " + currentProcess.pid());
+        ProcessHandle.Info currentProcessInfo = currentProcess.info();
+        log.info("Info: " + currentProcessInfo);
+    }
+}
